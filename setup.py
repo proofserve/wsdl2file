da1a240803b24be112efff7ac6e5cbdcc09aaa2b
Tyler MacDonald <tyler@macdonald.name>
2024-08-06 19:15:50 -0400

    Compile a WSDL file with multiple includes into a single file
    The `wsdl2file` tool takes a WSDL, walks through all of its includes,
    and compiles it down to a single file. It handles quirks with Microsoft
    WCF such as it sometimes using backslashes instead of slashes, and also
    comes with a `--zeep` flag that attempts to change the schema to make
    it possible for Zeep to use it (which amounts to converting NIEM
    abstract types into concrete types).
    
    I wrote this after spending hours on the internet looking at other
    peoples' solutions and finding that none of them could deal with the
    insane complexity that is the ECF5 standard. When I get a few spare
    cycles, I am going to clean this up (add a few unit tests, some more
    documentation, etc) and release it as open source so that future people
    that want to do modern programming against bloated government
    webservices don't have to go through the same hell that I did.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+setup(
+    name="wsdl2file",
+    version="0.1",
+    author="Tyler MacDonald",
+    author_email="tyler@proofserve.com",
+    description="Compile a WCF WSDL down to a single file",
+    license="MIT License",
+    install_requires=read_requirements("requirements.txt"),
+    entry_points={
+        'console_scripts': [
+            'wsdl2file = wsdl2file.cmd:main'
+        ]
+    },
+    packages = ["wsdl2file"]
+)
